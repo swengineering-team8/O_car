7b1e5c22805c53cd64cc012ccc21b66273936b01
PhamTung <phamtritungtdtt@gmail.com>
2022-05-24 00:34:19 +0900

    add Login func And Register func

--- app.js
+++ app.js
@@ -3,32 +3,48 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var flash = require('express-flash');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+
+var mysql = require('mysql');
+var connection = require('./models/database');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var authRouter = require('./routes/auth');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
+app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.use(session({
+  secret: '123456cat',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { maxAge: 60000 }
+}))
+
+app.use(flash());
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/auth', authRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
---
+++ models/database.js
@@ -0,0 +1,19 @@
+var mysql = require('mysql');
+
+//Database를 만들었으면 정보를 추가해 주세요
+var db = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password: 'Tungbg@97',
+    database: 'tutorial'
+});
+
+db.connect(function(error){
+    if(!!error){
+      console.log(error);
+    }else{
+      console.log('Connected!:)');
+    }
+  });
+
+module.exports = db;
