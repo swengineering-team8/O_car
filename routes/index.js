7e2c135a6f46b905b7afbab2ac1e115b1e445ab4
PhamTung <phamtritungtdtt@gmail.com>
2022-06-06 20:03:29 +0900

    update Search Bar with MySQL

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,32 @@
 var express = require('express');
 var router = express.Router();
 var connection = require('../models/database');
+var multer = require('multer');
+var path = require('path');
+
+//Image Loading
+
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'public/images/');
+  },
+  filename: function (req, file, cb) {
+    const ext = path.extname(file.originalname);
+    cb(null, path.basename(file.originalname, ext) + "-" + Date.now() + ext);
+  },
+});
+
+var upload = multer({ storage: storage });
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'O-Car' });
 });
 
 router.get('/search', function (req, res) {
-  var name = req.query.name;
+  var title = req.query.title;
 
-  var sql = "SELECT * FROM users WHERE userName LIKE '%" + name + "%'";
+  var sql = "SELECT * FROM board WHERE title LIKE '%" + title + "%'";
 
   connection.connect(function (err) {
     if (err) console.log("err: ", err);
